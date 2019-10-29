1417bca28948b12067f5442cff4ae36609c011bd
dalexymontoya <52118414+dalexymontoya@users.noreply.github.com>
2019-10-29 13:43:43 -0500

    Add files via upload

---
+++ walk.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <random>
+#include <stdlib.h>
+#include <cmath>
+
+int main(){
+  int i;
+  float pi = 2.0*asin(1.0);
+  float theta = 0.0;
+  float x = 0.0;
+  float y = 0.0;
+  float xi = 0.0;
+  
+  srand48(8);
+    
+    
+  
+  while (xi<100){  
+      
+      
+    theta = drand48()*2.0*pi;
+    x = x + cos(theta);
+    y = y + sin(theta);
+      
+    xi = sqrt(x*x+y*y);
+    std::cout << x << " " << y << std::endl;
+  }
+  
+  return 0;
+}
+
